a5a7727b59792207e42100aab3db9a87c6c2e985
Vitaliy Bobrov <bobrov1989@gmail.com>
2019-03-12 20:38:57 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ally-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'ng-a11y';
+}
