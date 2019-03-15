1730355a37e51ead8037ec7059850e260e9ea1ed
Vitaliy Bobrov <bobrov1989@gmail.com>
2019-03-16 00:23:43 +0200

    Add axe tests

--- src/app/axe-test/dropdown/dropdown.component.spec.ts
+++ src/app/axe-test/dropdown/dropdown.component.spec.ts
@@ -1,3 +1,5 @@
+const axe = require('axe-core');
+import { DebugElement } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DropdownComponent } from './dropdown.component';
@@ -5,6 +7,8 @@ import { DropdownComponent } from './dropdown.component';
 describe('DropdownComponent', () => {
   let component: DropdownComponent;
   let fixture: ComponentFixture<DropdownComponent>;
+  let dropdownDe: DebugElement;
+  let dropdownEl: HTMLElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -18,10 +22,39 @@ describe('DropdownComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(DropdownComponent);
     component = fixture.componentInstance;
+    dropdownDe = fixture.debugElement;
+    dropdownEl = dropdownDe.nativeElement;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should be accessible on initial state', (done) => {
+    axe.run(dropdownEl, (err, result) => {
+      if (result.violations.length > 0) {
+        console.log(JSON.stringify(result.violations, null, 4));
+      }
+
+      expect(err).toBe(null);
+      expect(result.violations.length).toBe(0);
+      done();
+    });
+  });
+
+  it('should be accessible when expanded', (done) => {
+    component.toggle.nativeElement.click();
+    fixture.detectChanges();
+
+    axe.run(dropdownEl, (err, result) => {
+      if (result.violations.length > 0) {
+        console.log(JSON.stringify(result.violations, null, 4));
+      }
+
+      expect(err).toBe(null);
+      expect(result.violations.length).toBe(0);
+      done();
+    });
   });
 });
